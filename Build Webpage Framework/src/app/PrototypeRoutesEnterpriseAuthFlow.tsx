eaa447338d24b833f7dfa4d340bffa020ddeee11
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 12:59:32 -0500

    Wire capability and impact prototype routes

--- Build Webpage Framework/src/app/PrototypeRoutesEnterpriseAuthFlow.tsx
+++ Build Webpage Framework/src/app/PrototypeRoutesEnterpriseAuthFlow.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import PrototypeRoutesAuthFlow from './PrototypeRoutesAuthFlow';
-import PrototypeRoutesArchitectureTree from './PrototypeRoutesArchitectureTree';
+import PrototypeRoutesCapabilityImpact from './PrototypeRoutesCapabilityImpact';
 
 const authRoutes = ['/login', '/signup', '/forgot-password'];
 const getRoute = () => window.location.hash.replace(/^#/, '').split('?')[0] || '/';
@@ -15,5 +15,5 @@ export default function PrototypeRoutesEnterpriseAuthFlow() {
   }, []);
 
   if (authRoutes.includes(route)) return <PrototypeRoutesAuthFlow />;
-  return <PrototypeRoutesArchitectureTree />;
+  return <PrototypeRoutesCapabilityImpact />;
 }
