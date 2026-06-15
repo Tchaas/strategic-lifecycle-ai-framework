a9f43e026d990d9b6e5236d56466cac81628da77
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 11:13:18 -0500

    Route signed-in prototype through enterprise flow

---
+++ Build Webpage Framework/src/app/PrototypeRoutesEnterpriseAuthFlow.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import PrototypeRoutesAuthFlow from './PrototypeRoutesAuthFlow';
+import PrototypeRoutesEnterpriseFlow from './PrototypeRoutesEnterpriseFlow';
+
+const authRoutes = ['/login', '/signup', '/forgot-password'];
+const getRoute = () => window.location.hash.replace(/^#/, '').split('?')[0] || '/';
+
+export default function PrototypeRoutesEnterpriseAuthFlow() {
+  const [route, setRoute] = useState(() => getRoute());
+
+  useEffect(() => {
+    const handleHashChange = () => setRoute(getRoute());
+    window.addEventListener('hashchange', handleHashChange);
+    return () => window.removeEventListener('hashchange', handleHashChange);
+  }, []);
+
+  if (authRoutes.includes(route)) return <PrototypeRoutesAuthFlow />;
+  return <PrototypeRoutesEnterpriseFlow />;
+}
