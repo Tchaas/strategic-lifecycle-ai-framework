ee4b5ba045922fbfe3629c9306b45d4b4e5678d5
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 10:15:32 -0500

    Wire invite prototype route

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import PrototypeRoutesValueStreams from "./app/PrototypeRoutesValueStreams.tsx";
+import PrototypeRoutesInvites from "./app/PrototypeRoutesInvites.tsx";
 import "./styles/index.css";
 import "./styles/prototype-overrides.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
-createRoot(document.getElementById("root")!).render(<PrototypeRoutesValueStreams />);
+createRoot(document.getElementById("root")!).render(<PrototypeRoutesInvites />);
 registerServiceWorker();
