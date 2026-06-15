d8327f202e0cd4567b07890f6c29ee112a4a4ba8
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 12:44:49 -0500

    Wire architecture hierarchy prototype flow

--- Build Webpage Framework/src/app/PrototypeRoutesEnterpriseAuthFlow.tsx
+++ Build Webpage Framework/src/app/PrototypeRoutesEnterpriseAuthFlow.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import PrototypeRoutesAuthFlow from './PrototypeRoutesAuthFlow';
-import PrototypeRoutesKeyActivitiesDashboard from './PrototypeRoutesKeyActivitiesDashboard';
+import PrototypeRoutesArchitectureTree from './PrototypeRoutesArchitectureTree';
 
 const authRoutes = ['/login', '/signup', '/forgot-password'];
 const getRoute = () => window.location.hash.replace(/^#/, '').split('?')[0] || '/';
@@ -15,5 +15,5 @@ export default function PrototypeRoutesEnterpriseAuthFlow() {
   }, []);
 
   if (authRoutes.includes(route)) return <PrototypeRoutesAuthFlow />;
-  return <PrototypeRoutesKeyActivitiesDashboard />;
+  return <PrototypeRoutesArchitectureTree />;
 }
