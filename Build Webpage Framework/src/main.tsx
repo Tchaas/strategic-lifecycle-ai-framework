de9958962b7697d4bf8911f8e5ac27b39bd0ed4c
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 09:56:20 -0500

    Use structured value stream prototype routes

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import PrototypeRoutesBuildFixed from "./app/PrototypeRoutesBuildFixed.tsx";
+import PrototypeRoutesValueStreams from "./app/PrototypeRoutesValueStreams.tsx";
 import "./styles/index.css";
 import "./styles/prototype-overrides.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
-createRoot(document.getElementById("root")!).render(<PrototypeRoutesBuildFixed />);
+createRoot(document.getElementById("root")!).render(<PrototypeRoutesValueStreams />);
 registerServiceWorker();
