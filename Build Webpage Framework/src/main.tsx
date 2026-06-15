59f70076946f3d3ead18bfa54214a2143be5e5b8
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 09:43:38 -0500

    Load prototype header overrides

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import PrototypeRoutesBuildFixed from "./app/PrototypeRoutesBuildFixed.tsx";
 import "./styles/index.css";
+import "./styles/prototype-overrides.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
 createRoot(document.getElementById("root")!).render(<PrototypeRoutesBuildFixed />);
