ee22f08c95113a9648f64be3f5822bcbba60b245
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 09:19:31 -0500

    Use corrected prototype lifecycle routes

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import PrototypeRoutes from "./app/PrototypeRoutes.tsx";
+import PrototypeRoutesCorrected from "./app/PrototypeRoutesCorrected.tsx";
 import "./styles/index.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
-createRoot(document.getElementById("root")!).render(<PrototypeRoutes />);
+createRoot(document.getElementById("root")!).render(<PrototypeRoutesCorrected />);
 registerServiceWorker();
