b2354b72103bda84019d36bb3e245e7ba7003354
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 11:09:14 -0500

    Wire scoped prototype storage boundary

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import PrototypeRoutesAuthGuard from "./app/PrototypeRoutesAuthGuard.tsx";
+import PrototypeRoutesScopedStorage from "./app/PrototypeRoutesScopedStorage.tsx";
 import "./styles/index.css";
 import "./styles/prototype-overrides.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
-createRoot(document.getElementById("root")!).render(<PrototypeRoutesAuthGuard />);
+createRoot(document.getElementById("root")!).render(<PrototypeRoutesScopedStorage />);
 registerServiceWorker();
