86d53812b589a9a07f20ed328b31da86db3bd8b4
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 10:42:25 -0500

    Wire strategic objectives prototype route

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import PrototypeRoutesInvites from "./app/PrototypeRoutesInvites.tsx";
+import PrototypeRoutesStrategicObjectives from "./app/PrototypeRoutesStrategicObjectives.tsx";
 import "./styles/index.css";
 import "./styles/prototype-overrides.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
-createRoot(document.getElementById("root")!).render(<PrototypeRoutesInvites />);
+createRoot(document.getElementById("root")!).render(<PrototypeRoutesStrategicObjectives />);
 registerServiceWorker();
