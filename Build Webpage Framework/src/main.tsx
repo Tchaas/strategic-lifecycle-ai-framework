d12af4aa0aa43dc79e2512df69744b865de2a4d4
Tchaas Alexander-Wright <tchaasawright@gmail.com>
2026-06-15 10:49:39 -0500

    Wire integrated dashboard invite flow

--- Build Webpage Framework/src/main.tsx
+++ Build Webpage Framework/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import PrototypeRoutesStrategicObjectives from "./app/PrototypeRoutesStrategicObjectives.tsx";
+import PrototypeRoutesIntegratedFlow from "./app/PrototypeRoutesIntegratedFlow.tsx";
 import "./styles/index.css";
 import "./styles/prototype-overrides.css";
 import { registerServiceWorker } from "./registerServiceWorker";
 
-createRoot(document.getElementById("root")!).render(<PrototypeRoutesStrategicObjectives />);
+createRoot(document.getElementById("root")!).render(<PrototypeRoutesIntegratedFlow />);
 registerServiceWorker();
